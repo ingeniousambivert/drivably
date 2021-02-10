7d63df8468fe73bcf29e0cd2cc865651e4201939
Monarch Maisuriya <monarchmaisuriya7600@gmail.com>
2021-02-10 18:32:48 +0530

    add car router

--- server/app.py
+++ server/app.py
@@ -1,11 +1,13 @@
-from fastapi import FastAPI
+from fastapi import Depends, FastAPI
 from server.routes.user import router as UserRouter
+from server.routes.car import router as CarRouter
 
 app = FastAPI()
 
 app.include_router(UserRouter, tags=["User"], prefix="/user")
+app.include_router(CarRouter, tags=["Car"], prefix="/car")
 
 
-@app.get("/", tags=["Root"])
+@ app.get("/", tags=["Root"])
 async def read_root():
-    return {"response": "Welcome to the Drivably API!"}
+    return "Welcome to the Drivably API!"
