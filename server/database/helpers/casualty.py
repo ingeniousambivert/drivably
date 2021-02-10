f0341c6d2b860216947f1c1d0179dfc2f287247f
Monarch Maisuriya <monarchmaisuriya7600@gmail.com>
2021-02-10 20:00:19 +0530

    fix helper methods

--- server/database/helpers/casualty.py
+++ server/database/helpers/casualty.py
@@ -46,24 +46,6 @@ async def retrieve_casualty(id: str) -> dict:
         return casualty_helper(casualty)
 
 
-# Update a casualty with a matching ID
-async def update_casualty(id: str, casualty_data: dict):
-    # Return false if an empty request body is sent.
-    if len(casualty_data) < 1:
-        return False
-
-    casualty = await casualties_collection.find_one({"_id": ObjectId(id)})
-
-    if casualty:
-        casualty_data.update({"updated_at": datetime.now()})
-        updated_casualty = await casualties_collection.update_one(
-            {"_id": ObjectId(id)}, {"$set": casualty_data}
-        )
-        if updated_casualty:
-            return True
-        return False
-
-
 # Delete a casualty from the database
 async def delete_casualty(id: str):
     casualty = await casualties_collection.find_one({"_id": ObjectId(id)})
