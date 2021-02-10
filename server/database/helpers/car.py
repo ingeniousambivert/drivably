3f194b3312accdff6735291f3079b6863e421506
Monarch Maisuriya <monarchmaisuriya7600@gmail.com>
2021-02-10 18:34:14 +0530

    add car helpers and update user helpers

---
+++ server/database/helpers/car.py
@@ -0,0 +1,73 @@
+from bson.objectid import ObjectId
+import motor.motor_asyncio
+from datetime import datetime
+
+MONGO_URI: str = "mongodb://localhost:27017"
+
+client = motor.motor_asyncio.AsyncIOMotorClient(MONGO_URI)
+
+database = client.drivably
+
+cars_collection = database.get_collection("cars")
+
+
+# helper methods
+
+def car_helper(car) -> dict:
+    return {
+        "id": str(car["_id"]),
+        "car_license": car["car_license"],
+        "car_name": car["car_name"],
+        "owner": car["owner"],
+        "drivers": car["drivers"],
+        "created_at": car["created_at"],
+    }
+
+
+# Retrieve all cars present in the database
+async def retrieve_cars():
+    cars = []
+    async for car in cars_collection.find():
+        cars.append(car_helper(car))
+    return cars
+
+
+# Add a new car into to the database
+async def add_car(car_data: dict) -> dict:
+    car_data.update({"created_at": datetime.now()})
+    car = await cars_collection.insert_one(car_data)
+    new_car = await cars_collection.find_one({"_id": car.inserted_id})
+    return car_helper(new_car)
+
+
+# Retrieve a car with a matching ID
+async def retrieve_car(id: str) -> dict:
+    car = await cars_collection.find_one({"_id": ObjectId(id)})
+    if car:
+        return car_helper(car)
+
+
+# Update a car with a matching ID
+async def update_car(id: str, car_data: dict):
+    # Return false if an empty request body is sent.
+    if len(car_data) < 1:
+        return False
+
+    car = await cars_collection.find_one({"_id": ObjectId(id)})
+
+    if car:
+        car_data.update({"updated_at": datetime.now()})
+        updated_car = await cars_collection.update_one(
+            {"_id": ObjectId(id)}, {"$set": car_data}
+        )
+        if updated_car:
+            return True
+        return False
+
+
+# Delete a car from the database
+async def delete_car(id: str):
+    car = await cars_collection.find_one({"_id": ObjectId(id)})
+    if car:
+        await cars_collection.delete_one({"_id": ObjectId(id)})
+        return True
---
+++ server/database/helpers/user.py
@@ -0,0 +1,75 @@
+from bson.objectid import ObjectId
+import motor.motor_asyncio
+from datetime import datetime
+
+MONGO_URI: str = "mongodb://localhost:27017"
+
+client = motor.motor_asyncio.AsyncIOMotorClient(MONGO_URI)
+
+database = client.drivably
+
+users_collection = database.get_collection("users")
+
+
+# helper methods
+
+def user_helper(user) -> dict:
+    return {
+        "id": str(user["_id"]),
+        "fullname": user["fullname"],
+        "facial_data": user["facial_data"],
+        "email": user["email"],
+        "password": user["password"],
+        "cars": user["cars"],
+        "phone": user["phone"],
+        "created_at": user["created_at"],
+    }
+
+
+# Retrieve all users present in the database
+async def retrieve_users():
+    users = []
+    async for user in users_collection.find():
+        users.append(user_helper(user))
+    return users
+
+
+# Add a new user into to the database
+async def add_user(user_data: dict) -> dict:
+    user_data.update({"created_at": datetime.now()})
+    user = await users_collection.insert_one(user_data)
+    new_user = await users_collection.find_one({"_id": user.inserted_id})
+    return user_helper(new_user)
+
+
+# Retrieve a user with a matching ID
+async def retrieve_user(id: str) -> dict:
+    user = await users_collection.find_one({"_id": ObjectId(id)})
+    if user:
+        return user_helper(user)
+
+
+# Update a user with a matching ID
+async def update_user(id: str, user_data: dict):
+    # Return false if an empty request body is sent.
+    if len(user_data) < 1:
+        return False
+
+    user = await users_collection.find_one({"_id": ObjectId(id)})
+
+    if user:
+        user_data.update({"updated_at": datetime.now()})
+        updated_user = await users_collection.update_one(
+            {"_id": ObjectId(id)}, {"$set": user_data}
+        )
+        if updated_user:
+            return True
+        return False
+
+
+# Delete a user from the database
+async def delete_user(id: str):
+    user = await users_collection.find_one({"_id": ObjectId(id)})
+    if user:
+        await users_collection.delete_one({"_id": ObjectId(id)})
+        return True
