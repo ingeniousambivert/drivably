61172481766c437374b484e26b6b451a75396a09
Monarch Maisuriya <monarchmaisuriya7600@gmail.com>
2021-02-10 19:50:01 +0530

    add more routers

--- server/app.py
+++ server/app.py
@@ -1,11 +1,18 @@
-from fastapi import Depends, FastAPI
+from fastapi import FastAPI
 from server.routes.user import router as UserRouter
 from server.routes.car import router as CarRouter
+from server.routes.bac import router as BacRouter
+from server.routes.activity import router as ActivityRouter
+from server.routes.casualty import router as CasualtyRouter
+
 
 app = FastAPI()
 
 app.include_router(UserRouter, tags=["User"], prefix="/user")
 app.include_router(CarRouter, tags=["Car"], prefix="/car")
+app.include_router(BacRouter, tags=["Car"], prefix="/bac")
+app.include_router(ActivityRouter, tags=["Car"], prefix="/activity")
+app.include_router(CasualtyRouter, tags=["Car"], prefix="/casualty")
 
 
 @ app.get("/", tags=["Root"])
