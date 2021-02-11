3064bec76b0f82ab25928445c9a517f5a38c379a
Monarch Maisuriya <monarchmaisuriya7600@gmail.com>
2021-02-11 16:18:01 +0530

    Add app dir

---
+++ server/app/main.py
@@ -0,0 +1,20 @@
+from fastapi import FastAPI
+
+from server.routes.user import router as UserRouter
+from server.routes.car import router as CarRouter
+from server.routes.bac import router as BacRouter
+from server.routes.activity import router as ActivityRouter
+from server.routes.casualty import router as CasualtyRouter
+
+app = FastAPI()
+
+app.include_router(UserRouter, tags=["User"], prefix="/user")
+app.include_router(CarRouter, tags=["Car"], prefix="/car")
+app.include_router(BacRouter, tags=["Blood Alcohol Content"], prefix="/bac")
+app.include_router(ActivityRouter, tags=["Activity"], prefix="/activity")
+app.include_router(CasualtyRouter, tags=["Casualty"], prefix="/casualty")
+
+
+@app.get("/", tags=["Root"])
+async def read_root():
+    return "Welcome to the Drivably API!"
