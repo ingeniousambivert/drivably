46f46c35bea9798e23f9d1fdde18540f5bf0bebc
Monarch Maisuriya <monarchmaisuriya7600@gmail.com>
2021-02-12 20:52:30 +0530

    add validate in router

--- server/routes/auth_router.py
+++ server/routes/auth_router.py
@@ -2,10 +2,14 @@ from fastapi import Body, APIRouter
 from fastapi.encoders import jsonable_encoder
 from fastapi.security import HTTPBasicCredentials
 from passlib.context import CryptContext
-from server.auth.validate import validate_login
+#from server.auth.validate import validate_login
 from server.auth.jwt_handler import signJWT
 
 
+from fastapi.security import HTTPBasicCredentials
+from passlib.context import CryptContext
+
+from server.database.helpers.user_helper import users_collection
 from server.database.controllers.user_controller import add_user
 
 from server.database.models.user_model import (
@@ -15,27 +19,32 @@ from server.database.models.user_model import (
 )
 
 router = APIRouter()
-
 hash_helper = CryptContext(schemes=["bcrypt"])
 
+# SIGNUP user
 
-# SIGNUP a user
+
 @router.post("/signup", response_description="user signed up")
 async def signup_user(user: UserSchema = Body(...)):
-    # TODO: Perform validation to check if user exists.
-    # hash user password before saving to database
+
+    user_email = users_collection.find_one({"email":  user.email})
+    if(user_email):
+        return ErrorResponseModel("Conflict", 409, "Email already exists")
+
     user.password = hash_helper.encrypt(user.password)
     user = jsonable_encoder(user)
     new_user = await add_user(user)
     return ResponseModel(new_user, "user added successfully.")
 
 
-# SIGNIN a user
+# SIGNIN user
 @router.post("/signin", response_description="user signed in")
-async def signin_user(user: HTTPBasicCredentials = Body(...)):
-    if validate_login(user):
-        return {
-            "email": user.username,
-            "access_token": signJWT(user.username)
-        }
-    return ErrorResponseModel("Incorrect email or password")
+async def signin_user(user_credentials: HTTPBasicCredentials = Body(...)):
+    user = await users_collection.find_one({"email": user_credentials.username}, {"_id": 0})
+    if (user):
+        password = hash_helper.verify(
+            user_credentials.password, user["password"])
+        if (password):
+            return signJWT(user_credentials.username)
+
+        return ErrorResponseModel("NotAuthenticated", 401, "Incorrect email or password")
