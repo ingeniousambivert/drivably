189bbbb028ea339dca58671e27e6a3eb9a397fd9
Monarch Maisuriya <monarchmaisuriya7600@gmail.com>
2021-02-12 17:15:18 +0530

    add auth router

--- core/app.py
+++ core/app.py
@@ -1,18 +1,29 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, Depends
 
 from server.routes.user_router import router as UserRouter
+from server.routes.auth_router import router as AuthRouter
 from server.routes.car_router import router as CarRouter
 from server.routes.bac_router import router as BacRouter
 from server.routes.activity_router import router as ActivityRouter
 from server.routes.casualty_router import router as CasualtyRouter
 
+from server.auth.jwt_bearer import JWTBearer
+
+token_listener = JWTBearer()
+
 app = FastAPI()
 
-app.include_router(UserRouter, tags=["User"], prefix="/user")
-app.include_router(CarRouter, tags=["Car"], prefix="/car")
-app.include_router(BacRouter, tags=["Blood Alcohol Content"], prefix="/bac")
-app.include_router(ActivityRouter, tags=["Activity"], prefix="/activity")
-app.include_router(CasualtyRouter, tags=["Casualty"], prefix="/casualty")
+app.include_router(AuthRouter, tags=["Auth"])
+app.include_router(UserRouter, tags=[
+                   "User"], prefix="/user", dependencies=[Depends(token_listener)])
+app.include_router(
+    CarRouter, tags=["Car"], prefix="/car", dependencies=[Depends(token_listener)])
+app.include_router(BacRouter, tags=[
+                   "Blood Alcohol Content"], prefix="/bac", dependencies=[Depends(token_listener)])
+app.include_router(ActivityRouter, tags=[
+                   "Activity"], prefix="/activity", dependencies=[Depends(token_listener)])
+app.include_router(CasualtyRouter, tags=[
+                   "Casualty"], prefix="/casualty", dependencies=[Depends(token_listener)])
 
 
 @app.get("/", tags=["Root"])
