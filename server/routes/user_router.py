e8e809cda26f13ac4e527cb15896e107349734cd
Monarch Maisuriya <monarchmaisuriya7600@gmail.com>
2021-02-13 18:26:51 +0530

    BUG:user_router:PUT

--- server/routes/user_router.py
+++ server/routes/user_router.py
@@ -1,16 +1,16 @@
-from fastapi import Body, APIRouter
-
+from server.database.models.user_model import (
+    ErrorResponseModel,
+    ResponseModel,
+    UpdateUserModel,
+)
 from server.database.controllers.user_controller import (
     delete_user,
     retrieve_user,
     retrieve_users,
     update_user,
 )
-from server.database.models.user_model import (
-    ErrorResponseModel,
-    ResponseModel,
-    UpdateUserModel,
-)
+from fastapi import Body, APIRouter
+
 
 router = APIRouter()
 
@@ -35,23 +35,26 @@ async def get_user(id):
 
 # UPDATE a user
 @router.put("/{id}")
-async def update_user(id: str, req: UpdateUserModel = Body(...)):
-    req = {key: value for key, value in req.dict().items() if value is not None}
-    updated_user = await update_user(id, req)
-    if updated_user:
-        return ResponseModel(
-            "User with ID: {} updated successfully".format(id),
-            "User updated successfully",
-        )
-    return ErrorResponseModel(
-        "An error occurred",
-        404,
-        "There was an error updating the user data.",
-    )
+async def update_user(id: str, data: UpdateUserModel = Body(...)):
+    dict_data = dict(data)
+    for key, value in dict_data.items():
+        if value is not None:
+            updated = {key: value}
+            updated_user = await update_user(id, updated)
+            if updated_user:
+                return ResponseModel(
+                    "User with ID: {} updated successfully".format(id),
+                    "User updated successfully",
+                )
+            return ErrorResponseModel(
+                "An error occurred",
+                404,
+                "There was an error updating the user data.",
+            )
 
 
 # DELETE a user
-@router.delete("/{id}", response_description="User data deleted from the database")
+@ router.delete("/{id}", response_description="User data deleted from the database")
 async def delete_user(id: str):
     deleted_user = await delete_user(id)
     if deleted_user:
