204e88ba38b906013c9f3c4d507316b5d37e7003
Monarch Maisuriya <monarchmaisuriya7600@gmail.com>
2021-02-13 16:56:43 +0530

    add CORS

--- core/app.py
+++ core/app.py
@@ -1,5 +1,6 @@
 from fastapi import FastAPI, Depends
 from fastapi.security import OAuth2PasswordBearer
+from fastapi.middleware.cors import CORSMiddleware
 from server.routes.user_router import router as UserRouter
 from server.auth.auth_router import router as AuthRouter
 from server.routes.car_router import router as CarRouter
@@ -7,6 +8,19 @@ from server.routes.car_router import router as CarRouter
 
 oauth2_scheme = OAuth2PasswordBearer(tokenUrl='signin')
 app = FastAPI()
+
+origins = [
+    "http://localhost",
+    "http://localhost:8008",
+]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 app.include_router(AuthRouter, tags=["Auth"])
 app.include_router(UserRouter, tags=["User"],
