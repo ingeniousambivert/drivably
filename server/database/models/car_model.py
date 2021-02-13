26788e21eb93c508bf9f5c40d02d717ac41066b5
Monarch Maisuriya <monarchmaisuriya7600@gmail.com>
2021-02-13 15:41:12 +0530

    update car model

--- server/database/models/car_model.py
+++ server/database/models/car_model.py
@@ -1,4 +1,4 @@
-from typing import List, Optional
+from typing import List, Optional, Dict
 from pydantic import BaseModel, Field
 
 
@@ -7,7 +7,12 @@ class CarSchema(BaseModel):
     car_name: str = Field(...)
     owner: str = Field(...)
     drivers: List[str] = Field(...)
+    current_location: List[str] = Field(None)
+    alcohol_concentrations: List[Dict] = Field(None)
+    casualties: List[Dict] = Field(None)
+    activities: List[Dict] = Field(None)
     created_at: str = Field(None)
+    updated_at: str = Field(None)
 
     class Config:
         schema_extra = {
@@ -16,6 +21,26 @@ class CarSchema(BaseModel):
                 "car_name": "car-1",
                 "owner": "user-id-1",
                 "drivers": ["user-id-2", "user-id-3", "user-id-4"],
+                "current_location": ["40.758896", " - 73.985130"],
+                "alcohol_concentrations": [
+                    {
+                        "value": "21.09",
+                        "driver": "driver-id-1",
+                    }
+                ],
+                "casualties": [
+                    {
+                        "location": ["40.758896", " - 73.985130"],
+                        "driver": "driver-id-1",
+                    }
+                ],
+                "activities": [
+                    {
+                        "driver": "driver-id-1",
+                        "data": "drowsiness_alert"
+                    }
+                ]
+
             }
         }
 
@@ -33,6 +58,26 @@ class UpdateCarModel(BaseModel):
                 "car_name": "car-1",
                 "owner": "user-id-1",
                 "drivers": ["user-id-2", "user-id-3"],
+                "current_location": ["40.758896", " - 73.985130"],
+                "alcohol_concentrations": [
+                    {
+                        "value": "21.09",
+                        "driver": "driver-id-1",
+                    }
+                ],
+                "casualties": [
+                    {
+                        "location": ["40.758896", " - 73.985130"],
+                        "driver": "driver-id-1",
+                    }
+                ],
+                "activities": [
+                    {
+                        "driver": "driver-id-1",
+                        "data": "drowsiness_alert"
+                    }
+                ]
+
             }
         }
 
