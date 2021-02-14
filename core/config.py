0c92037b59e64521b9013633cad6f5fc9b391c62
Monarch Maisuriya <monarchmaisuriya7600@gmail.com>
2021-02-14 13:10:05 +0530

    remove config from core

--- core/config.py
+++
@@ -1,6 +0,0 @@
-MONGO_URI: str = "mongodb://localhost:27017"
-
-# to get a string like this run:
-# openssl rand -hex 32
-JWT_SECRET = "eeb7b4544c80a3b26cc87e0d76d664d7a2d9af75a8fa56818ffffb611e5ec4ea"
-JWT_ALGORITHM = "HS256"
