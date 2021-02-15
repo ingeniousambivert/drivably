eabb897dce52ac2a1f65ecbd93db99937a623c6f
Monarch Maisuriya <monarchmaisuriya7600@gmail.com>
2021-02-15 16:33:02 +0530

    update dataset configs

--- intelligence/face_recognition/recognize-picture.py
+++ intelligence/face_recognition/recognize-picture.py
@@ -1,6 +1,6 @@
 import face_recognition
-known_image = face_recognition.load_image_file("./known/elon.jpg")
-unknown_image = face_recognition.load_image_file("./unknown/jeff.jpg")
+known_image = face_recognition.load_image_file("./face/known/elon.jpg")
+unknown_image = face_recognition.load_image_file("./face/unknown/jeff.jpg")
 
 known_face_names = [
     "Elon Musk",
