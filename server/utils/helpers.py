e5a4af316d796c5f318835c668582e424d3fb078
Monarch Maisuriya <monarchmaisuriya7600@gmail.com>
2021-02-17 14:39:52 +0530

    update helper methods

--- server/utils/helpers.py
+++ server/utils/helpers.py
@@ -1,3 +1,10 @@
+import shutil
+import os
+from pathlib import Path
+from tempfile import NamedTemporaryFile
+from typing import Callable
+
+from fastapi import UploadFile
 from server.database.helpers.car_helper import cars_collection
 
 
@@ -6,3 +13,35 @@ async def check_car_exists(car_license: str):
     if car:
         return True
     return False
+
+
+def save_upload_file(upload_file: UploadFile) -> None:
+    try:
+        if not os.path.exists("data/"):
+            os.makedirs("data/")
+
+        file_location = f"data/{upload_file.filename}"
+        with open(file_location, "wb+") as file_object:
+            shutil.copyfileobj(upload_file.file, file_object)
+        return f"file {upload_file.filename} uploaded at {file_location}"
+    finally:
+        upload_file.file.close()
+
+
+def save_upload_file_tmp(upload_file: UploadFile) -> Path:
+    try:
+        suffix = Path(upload_file.filename).suffix
+        with NamedTemporaryFile(delete=False, suffix=suffix) as tmp:
+            shutil.copyfileobj(upload_file.file, tmp)
+            tmp_path = Path(tmp.name)
+    finally:
+        upload_file.file.close()
+    return tmp_path
+
+
+def handle_upload_file(upload_file: UploadFile, handler: Callable[[Path], None]) -> None:
+    tmp_path = save_upload_file_tmp(upload_file)
+    try:
+        handler(tmp_path)  # Do something with the saved temp file
+    finally:
+        tmp_path.unlink()  # Delete the temp file
