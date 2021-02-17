274038a9b2217867d9be77ee26c89952a0b3082e
Monarch Maisuriya <monarchmaisuriya7600@gmail.com>
2021-02-17 18:05:19 +0530

    add env support

--- server/utils/config.py
+++ server/utils/config.py
@@ -1,4 +1,22 @@
-MONGO_URI: str = "mongodb://localhost:27017"
-# to get a hex string run: openssl rand -hex 32
-JWT_SECRET: str = "eeb7b4544c80a3b26cc87e0d76d664d7a2d9af75a8fa56818ffffb611e5ec4ea"
-JWT_ALGORITHM: str = "HS256"
+from typing import Optional
+from pydantic import BaseSettings
+
+
+class AppConfig(BaseSettings):
+
+    openapi_url: str = "/openapi.json"
+
+    MAIL_USERNAME: Optional[str]
+    MAIL_PASSWORD: Optional[str]
+    MAIL_FROM: Optional[str]
+    MAIL_PORT: Optional[int]
+    MAIL_SERVER: Optional[str]
+    MONGO_URI: Optional[str]
+    JWT_SECRET: Optional[str]
+    JWT_ALGORITHM: Optional[str]
+
+    class Config:
+        env_file: str = ".env"
+
+
+configured = AppConfig()
