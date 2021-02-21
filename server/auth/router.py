b257c2e018c7905a1d0fde4a73e351b1d2aa52a6
Monarch Maisuriya <monarchmaisuriya7600@gmail.com>
2021-02-21 10:28:07 +0530

    update auth routes

--- server/auth/router.py
+++ server/auth/router.py
@@ -16,9 +16,46 @@ from server.database.models.user_model import (
 router = APIRouter()
 
 
-# SIGNUP user
-@router.post("/signup", response_description="user signed up")
-async def signup_user(user: UserSchema = Body(...)):
+# SIGN UP Owner
+@router.post("/owner/signup", response_description="Owner signed up")
+async def signup_owner(user: UserSchema = Body(...)):
+    email_exists = await check_user_exists(user.email)
+    encoded_user = dict()
+
+    if not email_exists:
+        encoded_user = await create_encoded_user(user)
+        encoded_user_with_token = add_token(encoded_user)
+        encoded_user_with_token["owner"] = True
+        return ResponseModel(encoded_user_with_token)
+
+    elif email_exists:
+        return ErrorResponseModel("Conflict", 409, "Email already exists")
+
+    return ErrorResponseModel("Server Error", 500, "Could not signup user")
+
+
+# SIGN IN Owner
+@router.post("/owner/signin", response_description="Owner signed in")
+async def signin_owner(credentials:  HTTPBasicCredentials = Body(...)):
+    validated = await validate_user(credentials)
+
+    if validated:
+        user_data = await retrieve_user_by_email(credentials.username)
+        if user_data["owner"]:
+            user_with_token = add_token(user_data)
+            return ResponseModel(user_with_token)
+
+        return ErrorResponseModel("Forbidden", 403, "Your account is forbidden")
+
+    elif not validated:
+        return ErrorResponseModel("NotAuthenticated", 401, "Incorrect email or password")
+
+    return ErrorResponseModel("Server Error", 500, "Could not sign in user")
+
+
+# SIGN UP Driver
+@router.post("/driver/signup", response_description="Driver signed up")
+async def signup_driver(user: UserSchema = Body(...)):
     email_exists = await check_user_exists(user.email)
     encoded_user = dict()
 
@@ -33,9 +70,9 @@ async def signup_user(user: UserSchema = Body(...)):
     return ErrorResponseModel("Server Error", 500, "Could not signup user")
 
 
-# SIGNIN user
-@router.post("/signin", response_description="user signed in")
-async def signin_user(credentials:  HTTPBasicCredentials = Body(...)):
+# SIGN IN Driver
+@router.post("/driver/signin", response_description="Driver signed in")
+async def signin_driver(credentials:  HTTPBasicCredentials = Body(...)):
     validated = await validate_user(credentials)
 
     if validated:
