3fec7b3b683bb3e036c427bc46bdf5df6ccfd503
Monarch Maisuriya <monarchmaisuriya7600@gmail.com>
2021-02-22 00:28:42 +0530

    update helper methods

--- server/utils/helpers.py
+++ server/utils/helpers.py
@@ -1,8 +1,6 @@
 import shutil
 import os
-from pathlib import Path
-from tempfile import NamedTemporaryFile
-from typing import Callable
+import uuid
 
 from fastapi import UploadFile
 from server.database.helpers.car_helper import cars_collection
@@ -15,33 +13,27 @@ async def check_car_exists(car_license: str):
     return False
 
 
-def save_upload_file(file_upload: UploadFile, file_name: str) -> None:
+def save_upload_file(file_upload: UploadFile, file_name: str, dir_location: str) -> None:
     try:
-        if not os.path.exists("data/faces/"):
-            os.makedirs("data/faces/")
+        if not os.path.exists(dir_location):
+            os.makedirs(dir_location)
 
-        file_location = f"data/faces/{file_name}_Face.png"
+        file_location = f"{dir_location}{file_name}"
         with open(file_location, "wb+") as file_object:
-            shutil.copyfileobj(file_upload.file, file_object)
-        return {"filename": file_name+"_Face", "location": file_location}
+            uploaded = shutil.copyfileobj(file_upload.file, file_object)
+            if not uploaded:
+                return("An error occurred")
+        return dir_location
+        # return {"filename": file_name, "file_location": file_location, "dir_location": dir_location}
     finally:
         file_upload.file.close()
 
 
-def save_upload_file_tmp(upload_file: UploadFile) -> Path:
-    try:
-        suffix = Path(upload_file.filename).suffix
-        with NamedTemporaryFile(delete=False, suffix=suffix) as tmp:
-            shutil.copyfileobj(upload_file.file, tmp)
-            tmp_path = Path(tmp.name)
-    finally:
-        upload_file.file.close()
-    return tmp_path
+def remove_dir_tree(path):
+    if shutil.rmtree(path):
+        return True
+    return False
 
 
-def handle_upload_file(upload_file: UploadFile, handler: Callable[[Path], None]) -> None:
-    tmp_path = save_upload_file_tmp(upload_file)
-    try:
-        handler(tmp_path)  # Do something with the saved temp file
-    finally:
-        tmp_path.unlink()  # Delete the temp file
+def gen_uuid():
+    return uuid.uuid4().hex
