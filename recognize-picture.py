51b688406e225bb1eb58583659d59c4f714d5732
Monarch Maisuriya <monarchmaisuriya7600@gmail.com>
2021-02-21 22:19:26 +0530

    update result output

--- recognize-picture.py
+++ recognize-picture.py
@@ -5,12 +5,12 @@ unknown_image = face_recognition.load_image_file("./unknown/jeff.jpg")
 known_encoding = face_recognition.face_encodings(known_image)[0]
 unknown_encoding = face_recognition.face_encodings(unknown_image)[0]
 
-result = face_recognition.compare_faces(
+result_array = face_recognition.compare_faces(
     [known_encoding], unknown_encoding)
 
-res = ''.join([str(elem) for elem in result])
+result_str = ''.join([str(elem) for elem in result_array])
 
-if res == "True":
+if result_str == "True":
     print("Known Face")
 else:
     print("Unknown Face")
