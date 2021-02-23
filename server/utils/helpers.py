cbc5a49a93a0e3f55632c00959af7f04f1079586
Monarch Maisuriya <monarchmaisuriya7600@gmail.com>
2021-02-23 09:46:30 +0530

    fix upload file helper

--- server/utils/helpers.py
+++ server/utils/helpers.py
@@ -1,6 +1,7 @@
 import shutil
 import os
 import uuid
+from typing import Dict
 
 from fastapi import UploadFile
 from server.services.cars.helpers.car_helper import cars_collection
@@ -16,18 +17,17 @@ async def check_car_exists(car_license: str):
     return False
 
 
-def save_upload_file(file_upload: UploadFile, file_name: str, dir_location: str) -> None:
+def save_upload_file(file_upload: UploadFile, file_name: str, dir_location: str) -> Dict:
     try:
         if not os.path.exists(dir_location):
             os.makedirs(dir_location)
 
         file_location = f"{dir_location}{file_name}"
         with open(file_location, "wb+") as file_object:
-            uploaded = shutil.copyfileobj(file_upload.file, file_object)
-            if not uploaded:
-                return("An error occurred")
-        return dir_location
-        # return {"filename": file_name, "file_location": file_location, "dir_location": dir_location}
+            shutil.copyfileobj(file_upload.file, file_object)
+            response = {"filename": file_name,
+                        "file_location": file_location, "dir_location": dir_location}
+        return response["dir_location"]
     finally:
         file_upload.file.close()
 
