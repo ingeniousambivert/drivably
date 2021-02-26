f15a924a783422e9311f17e11ba6d88d73c1f486
aditya0802 <adityanegandhi02@gmail.com>
2021-02-26 13:51:19 +0530

    Implemented map in mapScreen

--- lib/screens/dashboard/navigationScreens/mapScreen.dart
+++ lib/screens/dashboard/navigationScreens/mapScreen.dart
@@ -1,6 +1,12 @@
 import 'package:flutter/material.dart';
-
+import 'package:mapbox_gl/mapbox_gl.dart';
 import '../../../utils/storage/localStorage.dart';
+import '../../../utils/constants/consts.dart';
+
+final LatLngBounds sydneyBounds = LatLngBounds(
+  southwest: const LatLng(-34.022631, 150.620685),
+  northeast: const LatLng(-33.571835, 151.325952),
+);
 
 class MapScreen extends StatefulWidget {
   const MapScreen({Key key}) : super(key: key);
@@ -11,6 +17,29 @@ class MapScreen extends StatefulWidget {
 
 class _MapScreenState extends State<MapScreen> {
   String licenseNumber;
+  static final CameraPosition _kInitialPosition = const CameraPosition(
+    target: LatLng(-33.852, 151.211),
+    zoom: 11.0,
+  );
+  MapboxMapController mapController;
+  // ignore: unused_field
+  CameraPosition _position = _kInitialPosition;
+  // ignore: unused_field
+  bool _isMoving = false;
+  bool _compassEnabled = true;
+  CameraTargetBounds _cameraTargetBounds = CameraTargetBounds.unbounded;
+  MinMaxZoomPreference _minMaxZoomPreference = MinMaxZoomPreference.unbounded;
+  bool _rotateGesturesEnabled = true;
+  bool _scrollGesturesEnabled = true;
+  bool _tiltGesturesEnabled = true;
+  bool _zoomGesturesEnabled = true;
+  bool _myLocationEnabled = true;
+  // ignore: unused_field
+  bool _telemetryEnabled = true;
+  MyLocationTrackingMode _myLocationTrackingMode = MyLocationTrackingMode.None;
+  // ignore: unused_field
+  List<Object> _featureQueryFilter;
+  // Fill _selectedFill;
 
   @override
   void initState() {
@@ -21,8 +50,49 @@ class _MapScreenState extends State<MapScreen> {
     });
   }
 
+  void _onMapChanged() {
+    setState(() {
+      _extractMapInfo();
+    });
+  }
+
+  void _extractMapInfo() {
+    _position = mapController.cameraPosition;
+    _isMoving = mapController.isCameraMoving;
+  }
+
+  @override
+  void dispose() {
+    mapController.removeListener(_onMapChanged);
+    super.dispose();
+  }
+
+  // Creating Custom MapBox Widget
   @override
   Widget build(BuildContext context) {
+    final MapboxMap mapboxMap = MapboxMap(
+      accessToken: ACCESS_TOKEN,
+      onMapCreated: onMapCreated,
+      initialCameraPosition: _kInitialPosition,
+      trackCameraPosition: true,
+      compassEnabled: _compassEnabled,
+      cameraTargetBounds: _cameraTargetBounds,
+      minMaxZoomPreference: _minMaxZoomPreference,
+      styleString: "mapbox://styles/blackdevil98/cklagsos11wb717pnn1dsieyq",
+      rotateGesturesEnabled: _rotateGesturesEnabled,
+      scrollGesturesEnabled: _scrollGesturesEnabled,
+      tiltGesturesEnabled: _tiltGesturesEnabled,
+      zoomGesturesEnabled: _zoomGesturesEnabled,
+      myLocationEnabled: _myLocationEnabled,
+      myLocationTrackingMode: _myLocationTrackingMode,
+      myLocationRenderMode: MyLocationRenderMode.GPS,
+
+      // TODO : Implement Realtime Location here
+      // onUserLocationUpdated: (location) {
+      //   print(
+      //       "new location: ${location.position}, alt.: ${location.altitude}, bearing: ${location.bearing}, speed: ${location.speed}, horiz. accuracy: ${location.horizontalAccuracy}, vert. accuracy: ${location.verticalAccuracy}");
+      // },
+    );
     return Scaffold(
       body: SafeArea(
         child: Row(
@@ -30,7 +100,7 @@ class _MapScreenState extends State<MapScreen> {
             Expanded(
               flex: 1,
               child: Container(
-                height: MediaQuery.of(context).size.height / 9,
+                // height: MediaQuery.of(context).size.height / 9,
                 padding: EdgeInsets.symmetric(horizontal: 20, vertical: 20),
                 color: Colors.black,
                 child: Column(
@@ -44,6 +114,19 @@ class _MapScreenState extends State<MapScreen> {
                       licenseNumber,
                       style: TextStyle(color: Colors.white70, fontSize: 15),
                     ),
+                    SizedBox(
+                      height: 50.0,
+                    ),
+                    Center(
+                      child: Container(
+                        height: 400.0,
+                        width: 300.0,
+                        decoration: BoxDecoration(
+                          borderRadius: BorderRadius.circular(50.0),
+                        ),
+                        child: mapboxMap,
+                      ),
+                    ),
                   ],
                 ),
               ),
@@ -52,5 +135,15 @@ class _MapScreenState extends State<MapScreen> {
         ),
       ),
     );
+  }
+
+  void onMapCreated(MapboxMapController controller) {
+    mapController = controller;
+    mapController.addListener(_onMapChanged);
+    _extractMapInfo();
+
+    mapController.getTelemetryEnabled().then((isEnabled) => setState(() {
+          _telemetryEnabled = isEnabled;
+        }));
   }
 }
