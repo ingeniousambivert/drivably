f23c7987d904bc2e3f240de7a81bf58331a0baaf
Monarch Maisuriya <monarchmaisuriya7600@gmail.com>
2021-02-27 11:13:17 +0530

    update drowsiness_detection module

--- intelligence/drowsiness_detection/detect_live.py
+++ intelligence/drowsiness_detection/detect_live.py
@@ -13,47 +13,53 @@ def eye_aspect_ratio(eye):
     return ear
 
 
-thresh = 0.25
-frame_check = 20
-detect = dlib.get_frontal_face_detector()
-# Dat file is the crux of the code
-predict = dlib.shape_predictor(
-    "intelligence/drowsiness_detection/dataset/shape_predictor_68_face_landmarks.dat")
+def detect_drowsiness(detect: bool):
+    if detect:
+        thresh = 0.25
+        frame_check = 20
+        detect = dlib.get_frontal_face_detector()
+        # Dat file is the crux of the code
+        predict = dlib.shape_predictor(
+            "intelligence/drowsiness_detection/dataset/shape_predictor_68_face_landmarks.dat")
 
-(lStart, lEnd) = face_utils.FACIAL_LANDMARKS_68_IDXS["left_eye"]
-(rStart, rEnd) = face_utils.FACIAL_LANDMARKS_68_IDXS["right_eye"]
-cap = cv2.VideoCapture(0)
-flag = 0
-while True:
-    ret, frame = cap.read()
-    frame = imutils.resize(frame, width=450)
-    gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
-    subjects = detect(gray, 0)
-    for subject in subjects:
-        shape = predict(gray, subject)
-        shape = face_utils.shape_to_np(shape)  # converting to NumPy Array
-        leftEye = shape[lStart:lEnd]
-        rightEye = shape[rStart:rEnd]
-        leftEAR = eye_aspect_ratio(leftEye)
-        rightEAR = eye_aspect_ratio(rightEye)
-        ear = (leftEAR + rightEAR) / 2.0
-        leftEyeHull = cv2.convexHull(leftEye)
-        rightEyeHull = cv2.convexHull(rightEye)
-        cv2.drawContours(frame, [leftEyeHull], -1, (0, 255, 0), 1)
-        cv2.drawContours(frame, [rightEyeHull], -1, (0, 255, 0), 1)
-        if ear < thresh:
-            flag += 1
-            print(flag)
-            if flag >= frame_check:
-                cv2.putText(frame, "****************DROWSINESS ALERT!****************", (10, 30),
-                            cv2.FONT_HERSHEY_SIMPLEX, 0.7, (0, 0, 255), 2)
-                cv2.putText(frame, "****************DROWSINESS ALERT!****************", (10, 325),
-                            cv2.FONT_HERSHEY_SIMPLEX, 0.7, (0, 0, 255), 2)
-        else:
-            flag = 0
-    cv2.imshow("Frame", frame)
-    key = cv2.waitKey(1) & 0xFF
-    if key == ord("q"):
-        break
-cv2.destroyAllWindows()
-cap.stop()
+        (lStart, lEnd) = face_utils.FACIAL_LANDMARKS_68_IDXS["left_eye"]
+        (rStart, rEnd) = face_utils.FACIAL_LANDMARKS_68_IDXS["right_eye"]
+        cap = cv2.VideoCapture(0)
+        flag = 0
+        while True:
+            ret, frame = cap.read()
+            frame = imutils.resize(frame, width=450)
+            gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+            subjects = detect(gray, 0)
+            for subject in subjects:
+                shape = predict(gray, subject)
+                shape = face_utils.shape_to_np(
+                    shape)  # converting to NumPy Array
+                leftEye = shape[lStart:lEnd]
+                rightEye = shape[rStart:rEnd]
+                leftEAR = eye_aspect_ratio(leftEye)
+                rightEAR = eye_aspect_ratio(rightEye)
+                ear = (leftEAR + rightEAR) / 2.0
+                leftEyeHull = cv2.convexHull(leftEye)
+                rightEyeHull = cv2.convexHull(rightEye)
+                cv2.drawContours(frame, [leftEyeHull], -1, (0, 255, 0), 1)
+                cv2.drawContours(frame, [rightEyeHull], -1, (0, 255, 0), 1)
+                if ear < thresh:
+                    flag += 1
+                    print("Detecting")
+                    print(flag)
+                    if flag >= frame_check:
+                        cv2.putText(frame, "****************DROWSINESS ALERT!****************", (10, 30),
+                                    cv2.FONT_HERSHEY_SIMPLEX, 0.7, (0, 0, 255), 2)
+                        cv2.putText(frame, "****************DROWSINESS ALERT!****************", (10, 325),
+                                    cv2.FONT_HERSHEY_SIMPLEX,  0.7, (0, 0, 255), 2)
+                else:
+                    flag = 0
+            cv2.imshow("Frame", frame)
+            key = cv2.waitKey(1) & 0xFF
+            if key == ord("q"):
+                break
+        cv2.destroyAllWindows()
+        cap.stop()
+    print("Undetected")
+    return("Undetected")
