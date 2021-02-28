90959271942e81fca05f2bb9fc477e66fe8258f1
Monarch Maisuriya <monarchmaisuriya7600@gmail.com>
2021-02-28 15:46:58 +0530

    update uvicorn params

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
 import uvicorn
 
+
 if __name__ == "__main__":
-    uvicorn.run("core.app:app", host="0.0.0.0", port=8008, reload=True)
+    uvicorn.run("core.app:app", host="0.0.0.0",
+                port=8008, reload=True, log_level="info")
