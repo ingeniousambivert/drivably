1f0a802cd7954e33e747eeecf0032658cd99799b
Monarch Maisuriya <monarchmaisuriya7600@gmail.com>
2021-03-12 16:35:54 +0530

    update drowsiness_detection module

--- intelligence/drowsiness_detection/detect_live.py
+++ intelligence/drowsiness_detection/detect_live.py
@@ -1,9 +1,11 @@
 import dlib
+import redis
 import cv2
 from scipy.spatial import distance
 from imutils import face_utils
 
-from multiprocessing import Process, Event
+
+client = redis.StrictRedis(host="localhost", port=6379, db=0)
 
 
 def eye_aspect_ratio(eye):
@@ -14,52 +16,43 @@ def eye_aspect_ratio(eye):
     return ear
 
 
-class DrowsinessDetector(Process):
+def drowsiness_detector(Key):
 
-    running = Event()
+    monitor = True if client.get(Key) else False
+    if monitor:
+        print("Monitoring Started")
 
-    def stop_monitoring(self):
-        if self.running.is_set():
-            self.running.clear()
+    thresh = 0.25
+    frame_check = 18
+    detect = dlib.get_frontal_face_detector()
+    # Dat file is the crux of the code
+    predict = dlib.shape_predictor(
+        "intelligence/drowsiness_detection/dataset/shape_predictor_68_face_landmarks.dat")
 
-    def start_monitoring(self):
-        if self.running.is_set():
-            return
+    (lStart, lEnd) = face_utils.FACIAL_LANDMARKS_68_IDXS["left_eye"]
+    (rStart, rEnd) = face_utils.FACIAL_LANDMARKS_68_IDXS["right_eye"]
+    cap = cv2.VideoCapture(0)
+    flag = 0
 
-        self.running.set()
-        self.detect_drowsiness()
+    while monitor:
+        ret, frame = cap.read()
+        gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+        subjects = detect(gray, 0)
+        for subject in subjects:
+            shape = predict(gray, subject)
+            shape = face_utils.shape_to_np(shape)  # converting to NumPy Array
+            leftEye = shape[lStart:lEnd]
+            rightEye = shape[rStart:rEnd]
+            leftEAR = eye_aspect_ratio(leftEye)
+            rightEAR = eye_aspect_ratio(rightEye)
+            ear = (leftEAR + rightEAR) / 2.0
+            if ear < thresh:
+                flag += 1
+                print("Detecting - {}".format(flag))
+                if flag >= frame_check:
+                    print("ALERT - Drowsy")
 
-    def detect_drowsiness(self):
-        thresh = 0.25
-        frame_check = 18
-        detect = dlib.get_frontal_face_detector()
-        # Dat file is the crux of the code
-        predict = dlib.shape_predictor(
-            "intelligence/drowsiness_detection/dataset/shape_predictor_68_face_landmarks.dat")
-
-        (lStart, lEnd) = face_utils.FACIAL_LANDMARKS_68_IDXS["left_eye"]
-        (rStart, rEnd) = face_utils.FACIAL_LANDMARKS_68_IDXS["right_eye"]
-        cap = cv2.VideoCapture(0)
-        flag = 0
-        while self.running.is_set():
-            ret, frame = cap.read()
-            gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
-            subjects = detect(gray, 0)
-            for subject in subjects:
-                shape = predict(gray, subject)
-                shape = face_utils.shape_to_np(
-                    shape)  # converting to NumPy Array
-                leftEye = shape[lStart:lEnd]
-                rightEye = shape[rStart:rEnd]
-                leftEAR = eye_aspect_ratio(leftEye)
-                rightEAR = eye_aspect_ratio(rightEye)
-                ear = (leftEAR + rightEAR) / 2.0
-                if ear < thresh:
-                    flag += 1
-                    print("Detecting,{}".format(flag))
-                    if flag >= frame_check:
-                        print("ALERT - Drowsy")
-
-                else:
-                    flag = 0
-        cap.release()
+            else:
+                flag = 0
+    cap.release()
+    print("Monitoring Stopped")
