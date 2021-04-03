5ab5773f8910b9b9ffc344f97e7b1091df414f25
Monarch Maisuriya <monarchmaisuriya7600@gmail.com>
2021-04-03 17:20:48 +0530

    update client configs

--- routes/user_routes.py
+++ routes/user_routes.py
@@ -1,11 +1,9 @@
-
-from utils.config import (BASE_URL)
 from utils.client import(httpClient, redisClient)
 
 
 def authenticate_user(data):
     try:
-        response = httpClient.post(f"{BASE_URL}/owner/signin", json=data)
+        response = httpClient.post("owner/signin", json=data)
         token = response.json()["access_token"]
         auth_data = {"key": "access_token", "value": token}
         redisClient.set(auth_data["key"], auth_data["value"])
--- utils/client.py
+++ utils/client.py
@@ -1,5 +1,6 @@
 import httpx
 import redis
+from utils.config import (BASE_URL)
 
-httpClient = httpx.Client()
+httpClient = httpx.Client(base_url=BASE_URL)
 redisClient = redis.StrictRedis(host="localhost", port=6379, db=0)
