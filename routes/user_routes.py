ef5c75cb3714108f2142924160164e9060255a05
Monarch Maisuriya <monarchmaisuriya7600@gmail.com>
2021-04-04 11:59:28 +0530

    update client configs

--- routes/user_routes.py
+++ routes/user_routes.py
@@ -3,24 +3,26 @@ from utils.client import(httpClient, httpError,
 
 
 def authenticate_user(data):
-    try:
-        response = httpClient.post("/driver/signin", json=data)
-        token = response.json()["access_token"]
-        redisClient.set("access_token", token)
-        print("Successfully authenticated user, STATUS:{}".format(
-            response.status_code))
-        response.raise_for_status()
-    except httpError as exc:
-        print(f"An error occured for {exc.request.url} - {exc}")
+    with httpClient as client:
+        try:
+            response = client.post("/driver/signin", json=data)
+            token = response.json()["access_token"]
+            redisClient.set("access_token", token)
+            print("authenticate_user : {}".format(
+                response.status_code))
+            response.raise_for_status()
+        except httpError as exc:
+            print(f"An error occured for {exc.request.url} - {exc}")
 
 
 def get_users():
-    try:
-        if access_token is not None:
-            response = httpClient.get("/user/", headers=auth_headers)
-            print(response.json())
-            response.raise_for_status()
-        else:
-            print("User access token not found or expired")
-    except httpError as exc:
-        print(f"An error occured for {exc.request.url} - {exc}")
+    with httpClient as client:
+        try:
+            if access_token is not None:
+                response = client.get("/user/", headers=auth_headers)
+                print(response.json())
+                response.raise_for_status()
+            else:
+                print("User access token not found or expired")
+        except httpError as exc:
+            print(f"An error occured for {exc.request.url} - {exc}")
